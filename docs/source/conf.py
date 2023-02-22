6cc2dbc8eaf6c15acd36cfa9884ec9f65a167e5a
Yong Zhou <zyong06@gmail.com>
2023-02-22 21:14:32 +0800

    cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Megat'
+copyright = '2023, USTC'
+author = 'Yong Zhou'
 
 release = '0.1'
 version = '0.1.0'
